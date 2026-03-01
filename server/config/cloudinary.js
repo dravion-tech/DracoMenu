5a3a2d3ed113b8bd7af087bafc8dc744ae3d4fef
Akhilesh-Dev-coder <akhileshtiss8@gmail.com>
2026-03-01 13:05:49 +0530

    cloundinary update

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,5 +1,11 @@
 const cloudinary = require("cloudinary").v2;
 
+console.log("Cloudinary Config Check:", {
+  hasCloudName: !!process.env.CLOUDINARY_CLOUD_NAME,
+  hasApiKey: !!process.env.CLOUDINARY_API_KEY,
+  hasApiSecret: !!process.env.CLOUDINARY_API_SECRET,
+});
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
